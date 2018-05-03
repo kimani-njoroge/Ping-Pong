24dfe7adb63d46e8591fd9e0bf4f523f493fce84
kimani-njoroge <kimani064@gmail.com>
2018-05-03 14:30:09 +0300

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
      display();
       event.preventDefault();
   });
-
+//Business Logic
+  function display(){
+    $('#output').text('');
+    for(var i=1;i<=numberInput;i++){
+      if((i % 3==0) && (i % 5==0)){
+        $('#output').append('<li>pingpong</li>');
+      }
+      else if(i % 5==0){
+          $('#output').append('<li>pong</li>');
+      }
+      else if(i % 3==0){
+          $('#output').append('<li>ping</li>');
+      }
+      else{
+          $('#output').append('<li>'+i.toString()+'</li>');
+      }
+    }
+  }
 
 });
